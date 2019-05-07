061a36e5306ade20e2aaf0228775ea79c32973c8
cindychea <cindy_chea@hotmail.com>
2019-05-07 15:06:26 -0400

    finished exercise2 stretch

--- exercise2.py
+++ exercise2.py
@@ -19,6 +19,13 @@ elif rating_dramas >= 4:
 elif rating_comedies >= 4:
     print("You should watch {}.".format(comedy))
 elif rating_docs <= 3 and rating_dramas <= 3 and rating_comedies <=3:
-    
+    if rating_docs > rating_comedies and rating_docs > rating_dramas:
+        print("You should watch {}.".format(documentary))
+    elif rating_dramas > rating_docs and rating_dramas > rating_comedies:
+        print("You should watch {}.".format(drama))
+    elif rating_comedies > rating_docs and rating_comedies > rating_dramas:
+        print("You should watch {}.".format(comedy))
+    else:
+        print("You seem indifferent. Maybe you should try reading a book or going for a walk. Otherwise, any of the following movies might interest you: Becoming Warren Buffet, Black Panther, The Mask, or Silver Linings Playbook.")
 else:
     print("You should read The Book of M by Peng Shepherd.")
