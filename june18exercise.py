115e22af3581ea4a1a6eb027c0a75650645f82f7
cindychea <cindy_chea@hotmail.com>
2019-06-19 12:25:09 -0400

    completed june18 reinforcement exercise

---
+++ june18exercise.py
@@ -0,0 +1,36 @@
+# Given an array of strings and an integer k, your task is to return the longest string consisting of k consecutive strings from the array concatenated together.
+# longestConsecutive(['hi', 'marbles', 'mittens', 'bye', 'lorem', 'ipsum', 'to', 'a', 'hippocampus'], 3); // -> 'marblesmittensbye'
+# If there is a tie, return the first one.
+# longestConsecutive(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2); // --> "abigailtheta"
+# Return "" if the array is empty or if k is negative or larger than the length of the array.
+
+
+def longestConsecutive(array, integer):
+    if array == [] or integer > len(array) or integer < 0:
+        print('""')
+    else:
+        letters = []
+        # if you do not want duplicates concatenated (only unique words)
+        for word in set(array):
+        # if you are okay with duplicates being concatenated
+        # for word in array:
+            letters.append({'word': word, 'length': len(word)})
+        ordered = sorted(letters, key=lambda k: k['length'], reverse=True)
+        longest = ordered[0:integer]
+        consecutive = []
+        for item in longest:
+            consecutive.append(item['word'])
+        print(''.join(consecutive))
+
+
+longestConsecutive(['hi', 'marbles', 'mittens', 'bye', 'lorem', 'ipsum', 'to', 'a', 'hippocampus'], 3)
+
+# with this example, the longest consecutive string would be abagailabagail unless we only want unique values
+# this is why there is an option for either line 14 or line 16 above
+longestConsecutive(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2); 
+
+longestConsecutive([], 2); 
+
+longestConsecutive(['hi'], 2); 
+
+longestConsecutive(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], -2); 
